8a2d0d7f5e14506f64bc3f934dc985266faeb7d6
Lucy-Lole <lucy-lole@outlook.com>
2025-12-19 18:55:19 +0000

    Day 8 P1 done on example set

--- src/day8.rs
+++ src/day8.rs
@@ -1,96 +1,126 @@
 use std::u64;
-
 use anyhow::Result;
 
 trait DistanceCanBeMeasured {
-    fn measure_distance(&self, other: &Self) -> u64;
+    fn measure_distance(&self, other: &Self) -> i64;
 }
 
-struct Point<'a>{
-    x: u64,
-    y: u64,
-    z: u64,
-    circuit: Option<&'a mut Vec<&'a mut Point<'a>>>
+struct Point {
+    x: i64,
+    y: i64,
+    z: i64,
+    circuit_id: Option<u64>,
 }
 
-impl DistanceCanBeMeasured for Point<'_> {
-    fn measure_distance(&self, other: &Self) -> u64 {
-        return self.x.abs_diff(other.x) + self.y.abs_diff(other.y) + self.z.abs_diff(other.z);
+ #[derive(Clone, PartialEq, Eq)]
+struct Circuit {
+    circuit_id: u64,
+    points: Vec<usize>
+}
+
+struct PointPair {
+    p1: usize,
+    p2: usize,
+    distance: i64
+}
+
+impl DistanceCanBeMeasured for Point {
+    fn measure_distance(&self, other: &Self) -> i64 {
+        return ((self.x - other.x).pow(2) + (self.y - other.y).pow(2)+ (self.z - other.z).pow(2)).isqrt()
     }
 }
 
-impl PartialEq for Point<'_> {
+impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
         return self.x.eq(&other.x) && self.y.eq(&other.y) && self.z.eq(&other.z);
     }
 }
 
 pub fn run_day_eight(file_as_string: String) -> Result<String> {
-    let points = process_data(file_as_string);
+    let mut points = process_data(file_as_string);
 
-    let result_value = points.len();
+    let result_value = find_product_of_largest(1000, &mut points);
 
     return Ok(format!("Result: {result_value}"));
 }
 
-fn find_product_of_largest(iterations: u64, points: &Vec<Point>) -> u64 {
-    let mut circuits: Vec<&Vec<&mut Point>> = Vec::new();
-    let mut connections_made = 0;
+fn find_product_of_largest(iterations: u64, points: &mut Vec<Point>) -> u64 {
+    let mut next_circuit_id = 0;
+    let mut circuits: Vec<Circuit> = Vec::new();
+    let mut pairs: Vec<PointPair> = Vec::new();
+    let points_length = points.len();
 
-    for i in 0..points.len() {
-        if connections_made > iterations { break; }
+    for point_index in 0..points_length {
+        for compare_index in 0..points_length {
+            if point_index == compare_index { continue; }
+            //else if pairs.iter().any(|a| a.p1 == compare_index && a.p2 == point_index) { continue; };
 
-        let ref mut point = points[0];
-        let ref mut lowest_distance_point = Point{x:u64::MAX, y:u64::MAX, z:u64::MAX, circuit: Option::None};
-        let mut lowest_distance = point.measure_distance(&lowest_distance_point);
+            let distance = points[point_index].measure_distance(&points[compare_index]);
 
-        for ref mut other in points {
-            if point == *other { continue; }
-
-            let distance = point.measure_distance(other);
-
-            if distance < lowest_distance {
-                lowest_distance_point = other;
-                lowest_distance = distance;
-            }
+            pairs.push(PointPair { p1: point_index, p2: compare_index, distance });
         }
+    }
 
-        match point.circuit {
-            Some(circuit) => {
-                match lowest_distance_point.circuit {
-                    Some(other_circuit) => {
+    pairs.sort_by(|a, b| a.distance.cmp(&b.distance));
 
+    let mut matches_made = 1;
+
+    for i in 0..pairs.len() {
+        if matches_made >= iterations { break; }
+
+        let PointPair { p1, p2, distance: _} = pairs[i as usize];
+        match points[p1].circuit_id {
+            Some(circuit_id) => {
+                match points[p2].circuit_id {
+                    Some(other_circuit_id) => {
+                        if circuit_id == other_circuit_id { continue; }
+
+                        let other_points = circuits.iter_mut().find(|a| a.circuit_id == other_circuit_id).unwrap().points.clone();
+
+                        circuits.iter_mut().find(|a| a.circuit_id == other_circuit_id).unwrap().points.clear();
+
+                        circuits.iter_mut().find(|a| a.circuit_id == circuit_id).unwrap().points.extend(other_points);
+
+                        points[p2].circuit_id = Some(circuit_id);
                     },
-                    None => todo!(),
+                    None => {
+                        points[p2].circuit_id = Some(circuit_id);
+                        circuits.iter_mut().find(|a| a.circuit_id == circuit_id).unwrap().points.push(p2);
+                    }
                 }
             },
             None => {
-                match lowest_distance_point.circuit {
-                    Some(other_circuit) => {
-                        point.circuit = Some(other_circuit);
-                        other_circuit.push(point);
+                match points[p2].circuit_id {
+                    Some(other_circuit_id) => {
+                        points[p1].circuit_id = Some(other_circuit_id);
+                        circuits.iter_mut().find(|a| a.circuit_id == other_circuit_id).unwrap().points.push(p1);
                     },
                     None => {
-                        let mut new_circuit = Vec::new();
-                        new_circuit.push(point);
-                        new_circuit.push(lowest_distance_point);
-                        circuits.push(&new_circuit);
-                        point.circuit = Some(&mut new_circuit);
-                        lowest_distance_point.circuit = Some(&mut new_circuit);
+                        let new_circuit = Circuit{circuit_id: next_circuit_id, points: vec![p1, p2] };
+                        points[p2].circuit_id = Some(next_circuit_id);
+                        points[p1].circuit_id = Some(next_circuit_id);
+
+                        next_circuit_id += 1;
+                        circuits.push(new_circuit);
                     }
                 }
             }
         }
+
+        matches_made += 1;
     }
 
-    circuits.sort_by(|a, b| a.len().cmp(&b.len()));
-    return (circuits[0].len() * circuits[1].len() * circuits[2].len()) as u64;
+
+    circuits.sort_by(|a,b| b.points.len().cmp(&a.points.len()));
+    return (circuits[0].points.len() * circuits[1].points.len() * circuits[2].points.len()) as u64;
 }
 
-fn process_data(file: String) -> Vec<Point<'static>> {
+fn process_data(file: String) -> Vec<Point> {
     return file
     .split('\n')
     .map(|r| r.split(',').collect())
-    .map(|row: Vec<&str>| Point{x: row[0].parse::<u64>().unwrap(), y: row[1].parse::<u64>().unwrap(),z: row[2].parse::<u64>().unwrap(), circuit: Option::None })
+    .map(|row: Vec<&str>| {
+        return Point{x: row[0].parse::<i64>().unwrap(), y: row[1].parse::<i64>().unwrap(),z: row[2].parse::<i64>().unwrap(), circuit_id: Option::None}; 
+    })
     .collect();
 }
